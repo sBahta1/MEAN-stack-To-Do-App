55ec85067feb45d641cc9fbd06f975fb7d408c4c
Sam Bahta <smabahta37@gmail.com>
2018-08-11 15:47:56 -0500

    html basic structure completed

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 // require express
 const express = require( 'express' );
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 //body parser
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+//connecting mongoose and mongodb
+const mongoose = require('mongoose');
+mongoose.connect(mongoURI, {useNewUrlParser: true});
+mongoose.connection.on('open', () => {
+    console.log('Connected to Mongo');
+});
+mongoose.connection.on('error', (error) => {
+    console.log('ERROR CONNECTING TO MONGO', error);
+});
+
 // Static files
 app.use(express.static('server/public'));
 
