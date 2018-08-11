c7649b3c536cbd119d55f2e9af93cd4be242234d
Sam Bahta <smabahta37@gmail.com>
2018-08-11 16:36:02 -0500

    First POST request route to DB finished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,24 @@ myApp.controller( 'HoneyController', function($http){
     let vm = this;
     vm.toDoArr = [];
     
-
+    vm.submitNewTask = function () {
+        let newTask = {
+            task: vm.newTaskInput,
+            completed: false
+        }
+        console.log('this is your new task', newTask);
+        $http({
+            method:'POST',
+            url:'/ToDoList',
+            data:newTask
+        }).then(function(resopnse){
+            console.log('Success!',response.data);
+            //GET FUNCTION  GOES HERE
+        }).catch(function(error){
+            alert('Unable to add task',error);
+            console.log(error);  
+        });
+    }//end vm.submitNewTask
 
 
 
--- server/server.js
+++ server/server.js
@@ -3,24 +3,46 @@ const express = require( 'express' );
 const app = express();
 const PORT = process.env.PORT || 5000;
 //body parser
-const bodyParser = require('body-parser');
+const bodyParser = require( 'body-parser' );
 app.use(bodyParser.json());
 
+//Schema
+const Schema = mongoose.Schema;
+
+const ToDoSchema = new Schema ({
+    task:{ type: String },
+    completed:{ type: Boolean },
+});
+const Task = mongoose.model( 'honeyDo', ToDoSchema )
+
 //connecting mongoose and mongodb
-const mongoose = require('mongoose');
-mongoose.connect(mongoURI, {useNewUrlParser: true});
-mongoose.connection.on('open', () => {
+const mongoose = require( 'mongoose' );
+mongoose.connect( mongoURI, { useNewUrlParser: true } );
+mongoose.connection.on( 'open', () => {
     console.log('Connected to Mongo');
 });
-mongoose.connection.on('error', (error) => {
-    console.log('ERROR CONNECTING TO MONGO', error);
+mongoose.connection.on( 'error', ( error ) => {
+    console.log( 'ERROR CONNECTING TO MONGO', error );
 });
 
 // Static files
-app.use(express.static('server/public'));
+app.use(express.static( 'server/public' ));
+
+//POST New Tasks
+app.post( '/toDoList', ( req,res ) => {
+    console.log( 'POST to /toDoList req.body =', req.body );
+    const dataFromClient = req.body;
+    const taskToAdd = new Task( dataFromClient );
+    taskToAdd.save().then( () => {
+        console.log( 'Task added', taskToAdd );
+        res.sendStatus( 200 );
+    }).catch( ( error ) => {
+        res.sendStatus( 500 );
+    })
+});
 
 
 
-app.listen(port,() => {
+app.listen( port,() => {
     console.log( 'Listening on port', port );    
 });
