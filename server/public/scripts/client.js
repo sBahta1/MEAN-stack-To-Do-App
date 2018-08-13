0fe9c89e83915ec8fb6ee47ed2ca860afd044a76
Sam Bahta <smabahta37@gmail.com>
2018-08-13 00:04:30 -0500

    Added filtering

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,12 @@
 console.log('js');
 
-const myApp = angular.module( 'myApp', [] );
-myApp.controller( 'HoneyController', function($http){
+const myApp = angular.module('myApp', []);
+myApp.controller('HoneyController', function ($http) {
     let vm = this;
     vm.toDoArr = [];
-    
-    
-//POST FUNCTION
+
+
+    //POST FUNCTION
     vm.submitNewTask = function () {
         let newTask = {
             task: vm.newTaskInput,
@@ -14,60 +14,60 @@ myApp.controller( 'HoneyController', function($http){
         }
         console.log('this is your new task', newTask);
         $http({
-            method:'POST',
-            url:'/toDoList',
-            data:newTask
-        }).then(function(response){
-            console.log('Success!',response.data);
-           getLists()
-        }).catch(function(error){
-            alert('Unable to add task',error);
-            console.log(error);  
+            method: 'POST',
+            url: '/toDoList',
+            data: newTask
+        }).then(function (response) {
+            console.log('Success!', response.data);
+            getLists()
+        }).catch(function (error) {
+            alert('Unable to add task', error);
+            console.log(error);
         });
     }//end vm.submitNewTask
 
-//GET FUNCTION
-function getLists() {
-    $http({
-        method: 'GET',
-        url:'/ToDoList'
-    }).then(function( response ){
-        console.log( 'Back from server with:', response.data );
-        vm.toDoArr = response.data
-    }).catch(function(error){
-        alert('Unable to get Lists', error);
-    });
-}
+    //GET FUNCTION
+    function getLists() {
+        $http({
+            method: 'GET',
+            url: '/ToDoList'
+        }).then(function (response) {
+            console.log('Back from server with:', response.data);
+            vm.toDoArr = response.data
+        }).catch(function (error) {
+            alert('Unable to get Lists', error);
+        });
+    }
 
-//Delete tasks
-vm.deleteTask = function (taskId) {
-    $http({
-        method: 'DELETE',
-        url:'/toDoList/'+taskId
-    }).then(function (response){
-        console.log('Task gone');
-        getLists();
-    }).catch(function(error){
-        console.log('Task not deleted', error);
-        alert('Unable to Delete Task', error);        
-    });
-}
+    //Delete tasks
+    vm.deleteTask = function (taskId) {
+        $http({
+            method: 'DELETE',
+            url: '/toDoList/' + taskId
+        }).then(function (response) {
+            console.log('Task gone');
+            getLists();
+        }).catch(function (error) {
+            console.log('Task not deleted', error);
+            alert('Unable to Delete Task', error);
+        });
+    }
 
-//Auto Populate DOM 
-getLists()
+    //Auto Populate DOM 
+    getLists()
 
-//completed tasks
-vm.taskFinished = function (taskId){
-    $http({
-        method:'PUT',
-        url:'/toDoList/'+taskId
-    }).then(function(response){
-        getLists();
-    }).catch(function(response){
-        console.log('unable to change entry');
-        alert('unable to change entry');    
-    })
-}
+    //completed tasks
+    vm.taskFinished = function (taskId) {
+        $http({
+            method: 'PUT',
+            url: '/toDoList/' + taskId
+        }).then(function (response) {
+            getLists();
+        }).catch(function (response) {
+            console.log('unable to change entry');
+            alert('unable to change entry');
+        })
+    }
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,75 +1,75 @@
 // require express
-const express = require( 'express' );
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mongoose = require( 'mongoose' );
+const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/todolists';
 //body parser
-const bodyParser = require( 'body-parser' );
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 //Schema
 const Schema = mongoose.Schema;
 
-const ToDoSchema = new Schema ({
-    task:{ type: String },
-    completed:{ type: Boolean },
+const ToDoSchema = new Schema({
+    task: { type: String },
+    completed: { type: Boolean },
 });
-const Task = mongoose.model( 'honeyDo', ToDoSchema )
+const Task = mongoose.model('honeyDo', ToDoSchema)
 
 //connecting mongoose and mongodb
 
-mongoose.connect(mongoURI,{useNewUrlParser:true} );
-mongoose.connection.on( 'open', () => {
+mongoose.connect(mongoURI, { useNewUrlParser: true });
+mongoose.connection.on('open', () => {
     console.log('Connected to Mongo');
 });
-mongoose.connection.on( 'error', ( error ) => {
-    console.log( 'ERROR CONNECTING TO MONGO', error );
+mongoose.connection.on('error', (error) => {
+    console.log('ERROR CONNECTING TO MONGO', error);
 });
 
 // Static files
-app.use(express.static( 'server/public' ));
+app.use(express.static('server/public'));
 
 //GET Lists
-app.get( '/toDoList',(req,res) => {
+app.get('/toDoList', (req, res) => {
     console.log('GET toDoLists');
-    Task.find( {} ).then((foundTasks) => {
+    Task.find({}).then((foundTasks) => {
         res.send(foundTasks);
     })
 });
 
 
 //POST New Tasks
-app.post( '/toDoList', ( req,res ) => {
-    console.log( 'POST to /toDoList req.body =', req.body );
+app.post('/toDoList', (req, res) => {
+    console.log('POST to /toDoList req.body =', req.body);
     const dataFromClient = req.body;
-    const taskToAdd = new Task( dataFromClient );
-    taskToAdd.save().then( () => {
-        console.log( 'Task added', taskToAdd );
-        res.sendStatus( 200 );
-    }).catch( ( error ) => {
-        res.sendStatus( 500 );
+    const taskToAdd = new Task(dataFromClient);
+    taskToAdd.save().then(() => {
+        console.log('Task added', taskToAdd);
+        res.sendStatus(200);
+    }).catch((error) => {
+        res.sendStatus(500);
     })
 });
 
 //Delete tasks
-app.delete('/toDoList/:id',(req,res)=>{
-    Task.findByIdAndRemove(req.params.id).then((response)=>{
+app.delete('/toDoList/:id', (req, res) => {
+    Task.findByIdAndRemove(req.params.id).then((response) => {
         res.sendStatus(200);
-    }).catch((error)=>{
+    }).catch((error) => {
         res.sendStatus(500);
     });
 });
 
 //completed Tasks
-app.put('/toDoList/:id',(req,res)=>{
+app.put('/toDoList/:id', (req, res) => {
     console.log('update', req.params.id);
-    Task.findOne({_id: req.params.id}).then((foundTask)=>{
+    Task.findOne({ _id: req.params.id }).then((foundTask) => {
         console.log(foundTask);
         foundTask.completed = true;
-        foundTask.save().then((response)=>{
+        foundTask.save().then((response) => {
             res.sendStatus(200);
-        }).catch((error)=>{
+        }).catch((error) => {
             res.sendStatus(500);
         });
     });
@@ -77,6 +77,6 @@ app.put('/toDoList/:id',(req,res)=>{
 
 
 
-app.listen(PORT,() => {
-    console.log( 'Listening on port', PORT );    
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT);
 });
